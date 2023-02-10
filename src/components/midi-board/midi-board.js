6070d70cfacfd591230e959941e6235f814809ea
ngnw <nnnwankwo57@gmail.com>
2023-02-09 18:53:03 -0700

    small changes

--- src/components/midi-board/midi-board.js
+++ src/components/midi-board/midi-board.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import pause from '../../assets/track-status/pause.png';
 import bass from '../../assets/TriSamples - 808 Trapstep Pack Vol 1/808 Bass/808 Bass A Overdrive .wav';
 import './midi-board.css';
 
